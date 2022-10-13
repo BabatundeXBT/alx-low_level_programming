8979c44738c9b54de6cd8ef888920b2489b548b0
BABATUNDEXBT <babatunde27@gmail.com>
2022-10-13 13:19:43 -0700

    first c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-Serves as the entry piont of the program
+ *
+ * Return:By default returns zero if no errors otherwise return non-zero value.
+ */
+int main(void)
+{
+	put("\"programming is like building a multilingual puzzle");
+	return(0);
+}

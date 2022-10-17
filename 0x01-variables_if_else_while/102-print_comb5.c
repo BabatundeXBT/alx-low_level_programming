6db0506902120d709a716c269b5242bff444b1b7
BABATUNDEXBT <babatunde27@gmail.com>
2022-10-17 15:13:47 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int c, i;
 
-	for (c = '0'; c <= '99'; c++)
+	for (c = '0'; c <= '9 9'; c++)
 	{
-		for (i = c + 1; i <= '99'; i++)
+		for (i = c + 1; i <= '9 9'; i++)
 		{
 			putchar('0' + c / 10);
 			putchar('0' + c % 10);

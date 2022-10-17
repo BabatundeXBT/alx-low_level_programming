fdf16e9cb28b1786ad2dd5b294ab1ae2ef349296
BABATUNDEXBT <babatunde27@gmail.com>
2022-10-17 08:33:05 -0700

    6-print_number.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Description:prnts char below 10
+ * Return:always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + 'o');
+	}
+	putchar('\n');
+	return (0);
+}

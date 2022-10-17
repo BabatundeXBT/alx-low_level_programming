db5cf06c931ba93bd4ec49f2b6c2905c4bac6c0f
BABATUNDEXBT <babatunde27@gmail.com>
2022-10-17 15:10:03 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main-Entry point
+ *
+ * Return:always 0 (success)
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = '0'; c <= '99'; c++)
+	{
+		for (i = c + 1; i <= '99'; i++)
+		{
+			putchar('0' + c / 10);
+			putchar('0' + c % 10);
+
+			putchar(' ');
+
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+
+			if (c == 98 && i == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

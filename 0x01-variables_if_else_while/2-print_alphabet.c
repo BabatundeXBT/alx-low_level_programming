ee6627fae756684d78fba59250cee15f2f9c6fa8
BABATUNDEXBT <babatunde27@gmail.com>
2022-10-17 07:56:01 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * description: prints alphabet in lowercase\n
- * Return: always 0 (success)
+ *main- Entry point
+ *Description:print alphabet in lowercase
+ *Return:Always 0 (success)
  */
-
 int main(void)
 {
-	char ch;
+	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-			putchar(ch);
+		putchar(ch);
 	}
-			putchar('\n');
+		putchar('\n');
 
 	return (0);
 }

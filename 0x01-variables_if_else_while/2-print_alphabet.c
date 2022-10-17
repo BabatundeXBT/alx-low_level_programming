1ce22bb506465cd5e166437106d2cfc5b18847d4
BABATUNDEXBT <babatunde27@gmail.com>
2022-10-17 03:49:31 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,17 +3,16 @@
  * main - Entry point
  * description: prints alphabet in lowercase\n
  * return always 0 (success)
-*/
+ */
 
 int main(void)
 {
 	int ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
-
 	{
 			putchar(ch);
 	}
 			putchar('\n');
 
-	return 0;
+	return (0);
 }

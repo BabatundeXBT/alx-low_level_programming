bed824af16511c0a4dad89b79eb74d3db25bad77
BABATUNDEXBT <babatunde27@gmail.com>
2022-10-18 13:00:14 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ *Return:always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a' ch <= 'z' ch++)
+		_putchar("%c\n", ch);
+
+	return (0);
+}

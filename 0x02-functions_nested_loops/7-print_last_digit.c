c06ed02fdfc37aceb8395814004c84810654fb10
BABATUNDEXBT <babatunde27@gmail.com>
2022-10-18 21:11:58 -0700

    last_digit number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#onclude "main.h"
+
+/**
+ *print_last_digit -> prints the last digit
+ *@n: the passed agrs
+ *Return: returns last digit
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
+}

4e7970d803fbc96e9b3c974f20926288b54e77cd
BABATUNDEXBT <babatunde27@gmail.com>
2022-10-18 17:52:03 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- *Description:print_alphabet in lower case - void function print_alphabet
+ *Description:print_alphabet in lower case - void function print_alphabet function 
  *
  *Return:always 0 (success)
  */
